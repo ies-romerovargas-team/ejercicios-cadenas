c1ed5292e95765b2f333a695d4b699ae5b742842
davidbermudez <davidbermudez@jerez.es>
2019-11-30 22:11:40 +0100

    Funcion esPalindromo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,38 +6,70 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String ANSI_RED="\u001B[31m";
-        String ANSI_GREEN="\u001B[32m";
-        String ANSI_PURPLE="\u001B[35m";
-        String ANSI_PURPLE_BACKGROUND="\u001B[45m";
         String ANSI_YELLOW="\u001B[33m";
         String ANSI_RESET="\u001B[0m";
         String ANSI_YELLOW_BACKGROUND="\u001B[43m";
+        String ANSI_BLACK_BACKGROUND="\u001B[40m";
+        String ANSI_BLACK="\u001B[30m";
+        String ANSI_BOLD="\u001B[01m";
+        String ANSI_NORMAL="\u001B[02m";
         Scanner sc = new Scanner(System.in);
         int opcion;
         boolean continuar = true;
         while (continuar) {
             System.out.println();
-            System.out.print(ANSI_PURPLE_BACKGROUND);
+            System.out.print(ANSI_BLACK_BACKGROUND);
             System.out.print(ANSI_YELLOW);
-            System.out.println("MENÚ EJERCICIOS ADICIONALES DE ARRAYS");
+            System.out.print(ANSI_BOLD);
+            System.out.print("  MENÚ EJERCICIOS CADENAS (I)                                                         ");
+            System.out.println(ANSI_RESET);
+
+            System.out.print(ANSI_BLACK_BACKGROUND);
+            System.out.print(" ");
+            System.out.print(ANSI_NORMAL);
             System.out.print(ANSI_YELLOW_BACKGROUND);
-            System.out.print(ANSI_PURPLE);
-            System.out.print("1. numeroEspacios");
-            System.out.print("\t2. numeroVocales");
+            System.out.print(ANSI_BLACK);
+            System.out.print(" 1. numeroEspacios");
+            System.out.print("\t\t2. numeroVocales");
             System.out.print("\t3. esPalindromo");
-            System.out.print("\t\t\t4. contiene");
-            System.out.println("\t\t\t\t5. repiteCaracter");
-            System.out.print("6. padLeft-padRight");
+            System.out.print("\t\t4. contiene          ");
+            System.out.print(ANSI_BLACK_BACKGROUND);
+            System.out.print(" ");
+            System.out.println(ANSI_RESET);
+
+            System.out.print(ANSI_BLACK_BACKGROUND);
+            System.out.print(" ");
+            System.out.print(ANSI_NORMAL);
+            System.out.print(ANSI_YELLOW_BACKGROUND);
+            System.out.print(ANSI_BLACK);
+            System.out.print(" 5. repiteCaracter ");
+            System.out.print("\t6. padLeft-padRight");
             System.out.print("\t7. quitaEspacios");
-            System.out.print("\t8. quitaEspaciosTrim");
-            System.out.print("\t\t9. sustituyeCaracter");
-            System.out.println("\t10. cuentaPalabras");
-            System.out.print(ANSI_PURPLE_BACKGROUND);
+            System.out.print("\t8. quitaEspaciosTrim ");
+            System.out.print(ANSI_BLACK_BACKGROUND);
+            System.out.print(" ");
+            System.out.println(ANSI_RESET);
+
+            System.out.print(ANSI_BLACK_BACKGROUND);
+            System.out.print(" ");
+            System.out.print(ANSI_NORMAL);
+            System.out.print(ANSI_YELLOW_BACKGROUND);
+            System.out.print(ANSI_BLACK);
+            System.out.print(" 9. sustituyeCaracter");
+            System.out.print("\t10. cuentaPalabras                                           ");
+            System.out.print(ANSI_BLACK_BACKGROUND);
+            System.out.print(" ");
+            System.out.println(ANSI_RESET);
+
+            System.out.print(ANSI_BLACK_BACKGROUND);
             System.out.print(ANSI_YELLOW);
-            System.out.println("0. Finalizar");
-            System.out.print(ANSI_RESET);
+            System.out.print(ANSI_BOLD);
+            System.out.print("  0. Finalizar                                                                        ");
+            System.out.println(ANSI_RESET);
+
+            System.out.print(ANSI_BOLD);
             System.out.print("Indique opcion: ");
+            System.out.print(ANSI_RESET);
             opcion = sc.nextInt();
             switch (opcion) {
                 case 0:
@@ -56,7 +88,17 @@ public class Main {
                 }
                 break;
                 case 3: {
-
+                    System.out.print("Introduzca una frase: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    if(esPalindromo(a))
+                    {
+                        System.out.println("Es Palíndromo");
+                    }
+                    else
+                    {
+                        System.out.println("No es Palíndromo");
+                    }
                 }
                 break;
                 case 4: {
@@ -89,9 +131,65 @@ public class Main {
         }
     }
 
+    public static String quitaAcentos(String a)
+    {
+        String b = "";
+        String compara = "áéíóúü";
+        for (int i = 0; i < a.length(); i++) {
+            switch (a.charAt(i))
+            {
+                case 'á':
+                    b = b + 'a';
+                    break;
+                case 'é':
+                    b = b + 'e';
+                    break;
+                case 'í':
+                    b = b + 'i';
+                    break;
+                case 'ó':
+                    b = b + 'o';
+                    break;
+                case 'ú': case 'ü':
+                    b = b + 'u';
+                    break;
+                default:
+                    b = b + a.charAt(i);
+            }
+        }
+        return b;
+    }
+
+    public static String quitaEspacios(String a)
+    {
+        String b = "";
+        for (int i = 0; i < a.length(); i++) {
+            if(a.charAt(i)!=' ')
+            {
+                b = b + a.charAt(i);
+            }
+        }
+        return b;
+    }
+
     public static boolean esPalindromo(String a)
     {
         //dabale arroz a la zorra el abad
+        // Eliminamos espacios
+        a = quitaEspacios(a);
+        // Transformamos a minúsculas
+        a = a.toLowerCase();
+        // Eliminamos acentos
+        a = quitaAcentos(a);
+        for (int i = 0; i < a.length()/2; i++) {
+            char b = a.charAt(i);
+            char c = a.charAt(a.length() - (i + 1));
+            if(a.charAt(i)!=a.charAt(a.length() - (i + 1)))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean encuentra(String b, char a)
