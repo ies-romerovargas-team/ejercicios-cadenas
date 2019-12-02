8a511ac594eafeb716e40679acae9a55ffb5fdbd
david <davidbermudez@jerez.es>
2019-12-02 12:40:21 +0100

    Finalización resto de funciones

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -21,7 +20,8 @@ public class Main {
             System.out.print(ANSI_BLACK_BACKGROUND);
             System.out.print(ANSI_YELLOW);
             System.out.print(ANSI_BOLD);
-            System.out.print("  MENÚ EJERCICIOS CADENAS (I)                                                         ");
+            System.out.print("  MENÚ EJERCICIOS CADENAS (I)");
+            System.out.print(repiteCaracter(' ', 57));
             System.out.println(ANSI_RESET);
 
             System.out.print(ANSI_BLACK_BACKGROUND);
@@ -56,7 +56,8 @@ public class Main {
             System.out.print(ANSI_YELLOW_BACKGROUND);
             System.out.print(ANSI_BLACK);
             System.out.print(" 9. sustituyeCaracter");
-            System.out.print("\t10. cuentaPalabras                                           ");
+            System.out.print("\t10. cuentaPalabras");
+            System.out.print(repiteCaracter(' ', 43));
             System.out.print(ANSI_BLACK_BACKGROUND);
             System.out.print(" ");
             System.out.println(ANSI_RESET);
@@ -64,7 +65,8 @@ public class Main {
             System.out.print(ANSI_BLACK_BACKGROUND);
             System.out.print(ANSI_YELLOW);
             System.out.print(ANSI_BOLD);
-            System.out.print("  0. Finalizar                                                                        ");
+            System.out.print("  0. Finalizar");
+            System.out.print(repiteCaracter(' ', 72));
             System.out.println(ANSI_RESET);
 
             System.out.print(ANSI_BOLD);
@@ -102,33 +104,162 @@ public class Main {
                 }
                 break;
                 case 4: {
-
+                    System.out.print("Introduzca una cadena: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    System.out.print("Introduzca un carácter: ");
+                    char b = sc.next().charAt(0);
+                    System.out.print("El caracter \'" + b + "\' ");
+                    if(contiene(a, b))
+                    {
+                        System.out.print("se encuentra en \"");
+                    }
+                    else
+                    {
+                        System.out.print("no se encuentra en \"");
+                    }
+                    System.out.println(a + "\"");
                 }
                 break;
                 case 5: {
-
+                    System.out.print("Introduzca un carácter: ");
+                    char a = sc.next().charAt(0);
+                    System.out.print("Introduzca un entero: ");
+                    int b = sc.nextInt();
+                    System.out.println(repiteCaracter(a, b));
                 }
                 break;
                 case 6: {
-
+                    System.out.print("Introduzca una cadena: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    System.out.print("Introduzca un entero: ");
+                    int b = sc.nextInt();
+                    System.out.println(padLeft(a,b));
+                    System.out.println(padRight(a,b));
                 }
                 break;
                 case 7: {
-
+                    System.out.print("Introduzca una cadena: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    System.out.println(quitaEspacios(a));
                 }
                 break;
                 case 8: {
-
+                    System.out.print("Introduzca una cadena: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    System.out.println(quitaEspaciosTrim(a));
                 }
                 break;
                 case 9: {
+                    System.out.print("Introduzca una cadena: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    System.out.print("Introduzca un carácter: ");
+                    char b = sc.next().charAt(0);
+                    System.out.print("Introduzca un carácter: ");
+                    char c = sc.next().charAt(0);
+                    System.out.println(a);
+                    System.out.println(sustituyeCaracter(a, b, c));
                 }
                 break;
+                case 10:
+                {
+                    System.out.print("Introduzca una cadena: ");
+                    String a = sc.nextLine();   // limpiar buffer
+                    a = sc.nextLine();
+                    System.out.println("Número de palabras: " + cuentaPalabras(a));
+                }
                 default: {
                     // opcion no válida
                 }
             }
         }
+    }
+
+    public static int cuentaPalabras(String a)
+    {
+        // Palabra: ristra de carácteres separados por cualquier cosa que no sea una letra
+        int palabras = 0;
+
+        // Explicación:
+        // Recorremos la cadena en búsqueda de carácteres que no sean letras
+        // Cuando encontramos algunos, ponemos la variable proc a true para indicar que se está procesando
+        // Mientras proc sea true, no contamos más palabras aunque haya carácteres que no sean letras
+        // Cuando encontramos la siguiente letra, proc pasa a ser false, con lo que habilitamos el algoritmo para contar más palabras
+
+        boolean proc = true;
+
+        // Añadimos un punto al final ya que no detectamos el salto de línea
+        a = a + ".";
+
+        for (int i = 0; i < a.length(); i++) {
+            if(!Character.isLetter(a.charAt(i))){
+                if(!proc) palabras++;
+                proc = true;
+            }
+            else
+            {
+                if(Character.isLetter(a.charAt(i))) proc = false;
+            }
+        }
+        return palabras;
+    }
+
+    public static String sustituyeCaracter(String a, char b, char c)
+    {
+        String aux = "";
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b )
+            {
+                aux = a.substring(0, i);
+                aux = aux + c + a.substring(i + 1);
+                a = aux;
+            }
+        }
+        return a;
+    }
+
+    public static String quitaEspaciosTrim(String a)
+    {
+        while(a.charAt(0)==' ')
+        {
+            a = a.substring(1);
+        }
+        while(a.charAt(a.length() - 1)==' ')
+        {
+            a = a.substring(0, a.length() - 1);
+        }
+        return a;
+    }
+
+    public static String padLeft(String a, int b)
+    {
+        int c = a.length(); // longitud original
+        for (int i = 0; i < (b - c); i++) {
+            a = "." + a;
+        }
+        return a;
+    }
+
+    public static String padRight(String a, int b)
+    {
+        int c = a.length(); // longitud original
+        for (int i = 0; i < (b - c); i++) {
+            a = a + ".";
+        }
+        return a;
+    }
+
+    public static String repiteCaracter(char a, int b)
+    {
+        String c = "";
+        for (int i = 0; i < b; i++) {
+            c = c + a;
+        }
+        return c;
     }
 
     public static String quitaAcentos(String a)
@@ -169,7 +300,8 @@ public class Main {
                 b = b + a.charAt(i);
             }
         }
-        return b;
+        a = b;
+        return a;
     }
 
     public static boolean esPalindromo(String a)
@@ -192,7 +324,7 @@ public class Main {
         return true;
     }
 
-    public static boolean encuentra(String b, char a)
+    public static boolean contiene(String b, char a)
     {
         for (int i = 0; i < b.length(); i++) {
             if (b.charAt(i) == a ) return true;
@@ -205,7 +337,7 @@ public class Main {
         int vc = 0;
         String compara = "aáAÁeéEÉiíIÍoóOÓuúüUÚÜ";
         for (int i = 0; i < a.length(); i++) {
-            if(encuentra(compara, a.charAt(i))) vc++;
+            if(contiene(compara, a.charAt(i))) vc++;
         }
         return vc;
     }
